23de24e9ab99779c98f1578aeccdfb997f81bedd
Daniel Riege <daniel.riege99@gmail.com>
2022-01-21 10:33:07 +0100

    Initial commit

---
+++ train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.9
+
+import gym
+import tinycarlo
+
+env = gym.make("tinycarlo-v0", fps=30, camera_resolution=(480//4,640//4), reward_red=-1)
+observation = env.reset()
+for episode in range(10):
+    while True:
+        env.render()
+        #action = env.action_space.sample()
+        action = [-1.0]
+        observation, reward, done, info = env.step(action)
+
+        if done:
+            observation = env.reset()
+            break
+env.close()
+
